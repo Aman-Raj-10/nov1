b2fc92febc802170e381590b092ce081c2c411a1
Aman-Raj-10 <amanrajx10@gmail.com>
2025-11-01 11:48:34 +0530

    1

---
+++ jsbasic.js
@@ -0,0 +1,19 @@
+// console.log("hello"); to print in js
+
+let namee = "Aman"
+let age = 19;
+
+//console.log(namee)
+//console.log(age)
+
+// console.log(namee, age);
+// console.table([namee, age]);
+
+// console.log(typeof namee)
+// console.log(typeof age)
+
+let a = 2;
+let b = "2";
+console.log(a == b);
+console.log(a === b);
+
